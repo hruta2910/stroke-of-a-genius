e5b708935b501c7b42a840fdd7c28f68949b7209
hruta <artisachdev1970@gmail.com>
2020-11-27 20:38:24 +0530

    hruta

--- sketch.js
+++ sketch.js
@@ -18,7 +18,8 @@ function setup(){
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
-
+    platform1 = new Ground(160,200,50,70);
+    
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
